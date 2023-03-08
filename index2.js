555ba8bed9bed5d235420e166999198fc84de36c
emamaria <emascript@gmail.com>
2023-03-08 16:42:41 +0100

    map and shuffle cards

--- index2.js
+++ index2.js
@@ -1,3 +1,61 @@
+
+// creo un array de las cartas de assets y uso ese array para mapear y 
+// añadir los cambios
+
+let cardsList = [
+  "2C", "2D", "2H", "2S",
+  "3C", "3D", "3H", "S",
+  "4C", "4D", "4H", "4S",
+  "5C", "5D", "5H", "5S",
+  "6C", "6D", "6H", "6S",
+  "7C", "7D", "7H", "7S",
+  "8C", "8D", "8H", "8S",
+  "9C", "9D", "9H", "9S",
+  "10C", "10D","10H", "10S",
+  "AC", "AD", "AH", "AS",
+  "JC", "JD", "JH", "JS",
+  "KC", "KD", "KH", "KS",
+  "QC", "QD", "QH", "QS",
+]
+
+
+
+let shuffledCards = cardsList
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+   
+console.log(shuffledCards)
+
+let cardsContainer = document.querySelector(".cards-container")
+
+
+
+shuffledCards.forEach(card =>{
+  
+  cardsContainer.innerHTML += `
+
+<div class="scene scene--card">
+      <div class="card">
+       <img
+          class="card__face card__face--front"
+          src="./cartas/red_back.png"
+          alt="back"
+        />
+        <img
+          class="card__face card__face--back purple"
+          src="./cartas/${card}.png"
+          alt="${card.slice(0, card.length - 1)}"
+        />"${card.slice(0, card.length - 1)}"
+      </div>
+    </div>
+`
+  
+}
+  )
+
+
+
 var cards = document.querySelectorAll('.card');
 // cuando le das la vuelta, se añade el is-flipped true
 let text = ""
